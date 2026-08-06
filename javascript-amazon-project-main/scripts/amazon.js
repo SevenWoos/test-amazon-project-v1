c8f3d3081d32dbca3a68609ca3bfa39645d852d7
SevenWoos <swenwu96@gmail.com>
2026-08-06 16:30:33 -0700

    Make add to cart button interactive.

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart" 
+        data-product-id="${product.id}">
         Add to Cart
       </button>
     </div>
@@ -54,7 +55,35 @@ products.forEach((product) => {
 
 });
 
-console.log(productsHTML);
+// Data Attribute lets you attach additonal information to an element. It has to use KEBAB CASE. Concerts to CAMEL CASE when accessed in JS. For example, data-product-id becomes productId in JS.
 
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML.innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push(
+          {
+            productId: productId,
+            quantity: 1
+          }
+        );
+      }
+
+      console.log(cart);
+    });
+});

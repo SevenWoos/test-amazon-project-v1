8933cd8b67aeb51897405ad0def56bca3888c6f0
SevenWoos <swenwu96@gmail.com>
2026-08-21 14:07:06 -0700

    Move addToCart into cart.js.

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
@@ -63,6 +63,17 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-cart-quantity')
+    .innerHTML = cartQuantity;
+};
+
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
     button.addEventListener('click', () => {
@@ -70,36 +81,8 @@ document.querySelectorAll('.js-add-to-cart')
       // Use shorthand destructuring to get the productId from the button's dataset.
       const {productId} = button.dataset;
 
-      let matchingItem;
-
-      cart.forEach((item) => {
-        if (productId === item.productId) {
-          matchingItem = item;
-        }
-      });
-
-      const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
-      const quantity = Number(quantitySelector.value);
-
-      if (matchingItem) {
-        matchingItem.quantity += quantity;
-      } else {
-        cart.push(
-          {
-            productId: productId,
-            quantity: quantity
-          }
-        );
-      }
-
-      let cartQuantity = 0;
-
-      cart.forEach((item) => {
-        cartQuantity += item.quantity;
-      });
-
-      document.querySelector('.js-cart-quantity')
-        .innerHTML = cartQuantity;
+      addToCart(productId);
+      updateCartQuantity();
 
        // 13j. Use DOM to get the added messsage for the product.
        const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);

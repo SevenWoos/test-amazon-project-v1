b01d6ca4f330d8d9a0f2667b2718ed0154cc9eb2
SevenWoos <swenwu96@gmail.com>
2026-08-22 19:36:51 -0700

    14f. Get all update links from page and add click event listeners. Console log each product id after attaching them to each update link (test).

--- 13-javascript-amazon-project/scripts/checkout.js
+++ 13-javascript-amazon-project/scripts/checkout.js
@@ -39,7 +39,8 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link" 
+              data-product-id="${matchingProduct.id}">
               Update
             </span>
             <span class="delete-quantity-link link-primary js-delete-link" 
@@ -123,3 +124,12 @@ function updateCartQuantity() {
 };
 
 updateCartQuantity();
+
+// 14f. Get all "Update" links from page and add "click" event listener.
+document.querySelectorAll('.js-update-link')
+  .forEach((updateLink) => {
+    updateLink.addEventListener('click', () => {
+      const productId = updateLink.dataset.productId;
+      console.log(productId);
+    });
+});

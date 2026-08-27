395bc4918606d42772bf12e9efe5c00ceadb32b0
SevenWoos <swenwu96@gmail.com>
2026-08-27 00:01:14 -0700

    14n. Added new quantity validation check.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -145,14 +145,20 @@ document.querySelectorAll('.js-save-link')
     saveLink.addEventListener('click', () => {
       const productId = saveLink.dataset.productId;
 
-      const container = document.querySelector(`.js-cart-item-container-${productId}`);
-      container.classList.remove('is-editing-quantity');
-
       // 14k. Get the quantity from clicking save.
       const quantityInput = document.querySelector(`.js-quantity-input-${productId}`);
       const newQuantity = Number(quantityInput.value);
 
+      // 14n. Make sure new quantity is not less than 0 or greater than 1000.
+      if (newQuantity < 0 || newQuantity >= 1000) {
+        alert('Quantity must be at least 0 and less than 1000.');
+        return;
+      };
+
       updateQuantity(productId, newQuantity);
+
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.classList.remove('is-editing-quantity');
 
       const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
       quantityLabel.innerHTML = newQuantity;

f99414c9ba00fb1cc8666c8a68b29acd7b683a82
SevenWoos <swenwu96@gmail.com>
2026-08-26 23:59:33 -0700

    14m. Update the new quantity in the HTML cart and checkout.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -35,7 +35,8 @@ cart.forEach((cartItem) => {
           </div>
           <div class="product-quantity">
             <span>
-              Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+              Quantity: <span class="quantity-label 
+                js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
             <span class="update-quantity-link link-primary js-update-link"
               data-product-id="${matchingProduct.id}">
@@ -152,5 +153,10 @@ document.querySelectorAll('.js-save-link')
       const newQuantity = Number(quantityInput.value);
 
       updateQuantity(productId, newQuantity);
+
+      const quantityLabel = document.querySelector(`.js-quantity-label-${productId}`);
+      quantityLabel.innerHTML = newQuantity;
+
+      updateCartQuantity();
     });
 });

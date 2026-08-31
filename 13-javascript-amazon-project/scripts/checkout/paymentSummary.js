4655f3817312fc39e32e6a5facc8a997d803a137
SevenWoos <swenwu96@gmail.com>
2026-08-31 13:50:14 -0700

    15i. Update Cart Quantity in paymentSummary (test).

--- 13-javascript-amazon-project/scripts/checkout/paymentSummary.js
+++ 13-javascript-amazon-project/scripts/checkout/paymentSummary.js
@@ -1,4 +1,4 @@
-import {cart} from '../../data/cart.js';
+import {cart, calculateCartQuantity} from '../../data/cart.js';
 import {getProduct} from '../../data/products.js';
 import {getDeliveryOption} from '../../data/deliveryOptions.js';
 import {formatCurrency} from '../utils/money.js';
@@ -21,6 +21,9 @@ export function renderPaymentSummary() {
   const taxCents = totalBeforeTaxCents * 0.1;
   const totalCents = totalBeforeTaxCents + taxCents;
 
+  // 15i. Update Cart Quantity in paymentSummary.
+  let cartQuantity = calculateCartQuantity();
+
   // View - generate the HTML with the data.
   const paymentSummaryHTML = `
     <div class="payment-summary-title">
@@ -28,7 +31,7 @@ export function renderPaymentSummary() {
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items (${cartQuantity}):</div>
       <div class="payment-summary-money">
         $${formatCurrency(productPriceCents)}
       </div>

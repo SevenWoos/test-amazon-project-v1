911b1d5860c8affdb0cc0d9355ce1f71b142eb96
SevenWoos <swenwu96@gmail.com>
2026-09-02 23:49:28 -0700

    16i. Test suite for removeFromCart() function (test).

--- 13-javascript-amazon-project/tests/data/cartTest.js
+++ 13-javascript-amazon-project/tests/data/cartTest.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, loadFromStorage} from '../../data/cart.js';
+import {addToCart, cart, loadFromStorage, removeFromCart} from '../../data/cart.js';
 
 describe('test suite: addToCart', () => {
   // beforeEach hook runs before each spec in this test suite. It is used to set up the environment for each test.
@@ -46,6 +46,52 @@ describe('test suite: addToCart', () => {
     expect(cart[0].quantity).toEqual(1);
 
     // Check if setItem received correct values.
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
+      productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+      quantity: 1, 
+      deliveryOptionId: '1'
+    }]));
+  });
+});
+
+
+// Test Suite for rmeoveFromCart.
+describe('test suite: removeFromCart', () => {
+  beforeEach(() => {
+    spyOn(localStorage, 'setItem');
+  });
+
+  it('removes a product from the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+        quantity: 1, 
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart.length).toEqual(0);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+    expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([]));
+  }); 
+
+  it('does nothing if the product is not in the cart', () => {
+    spyOn(localStorage, 'getItem').and.callFake(() => {
+      return JSON.stringify([{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
+        quantity: 1, 
+        deliveryOptionId: '1'
+      }]);
+    });
+    loadFromStorage();
+
+    removeFromCart('does-not-exist');
+    expect(cart.length).toEqual(1);
+    expect(cart[0].productId).toEqual('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
+    expect(cart[0].quantity).toEqual(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
     expect(localStorage.setItem).toHaveBeenCalledWith('cart', JSON.stringify([{
       productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6', 
       quantity: 1, 

5950ed5510816f8b9e16874421365da1d6225e7b
SevenWoos <swenwu96@gmail.com>
2026-09-02 23:06:52 -0700

    16f. afterEach hook to clear HTML after each spec (test).

--- 13-javascript-amazon-project/tests/checkout/orderSummaryTest.js
+++ 13-javascript-amazon-project/tests/checkout/orderSummaryTest.js
@@ -38,6 +38,12 @@ describe('test suite: renderOrderSummary', () => {
     renderOrderSummary();
   });
 
+  // afterEach hook to cleanup the HTML after each spec.
+  afterEach(() => {
+    // Clear the HTML
+    document.querySelector('.js-test-container').innerHTML = ``;
+  });
+
   it('displays the cart', () => {
     expect(
       document.querySelectorAll('.js-cart-item-container').length
@@ -50,9 +56,6 @@ describe('test suite: renderOrderSummary', () => {
     expect(
       document.querySelector(`.js-product-quantity-${productId2}`).innerText
     ).toContain('Quantity: 1');
-
-     // Clear the HTML
-    document.querySelector('.js-test-container').innerHTML = ``;
   });
 
   // Check delete links work
@@ -76,8 +79,5 @@ describe('test suite: renderOrderSummary', () => {
     // Is cart array updated.
     expect(cart.length).toEqual(1);
     expect(cart[0].productId).toEqual(productId2);
-
-     // Clear the HTML
-    document.querySelector('.js-test-container').innerHTML = ``;
   });
 });

ce5a9e167109b8b634a81dbe8f82a8c7634b4b74
SevenWoos <swenwu96@gmail.com>
2026-09-05 13:44:52 -0700

    17h. Appliance class.

--- javascript-amazon-project-main/data/products.js
+++ javascript-amazon-project-main/data/products.js
@@ -121,6 +121,29 @@ const object3 = {
 object3.method();
 */
 
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    return `
+      <a href="${this.instructionsLink}" target="_blank">
+        Instructions
+      </a>
+
+      <a href="${this.warrantyLink}" target="_blank">
+        Warranty
+      </a>
+    `;
+  }
+};
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -181,7 +204,10 @@ export const products = [
       "toaster",
       "kitchen",
       "appliances"
-    ]
+    ], 
+    type: "appliance", 
+    instructionsLink: "images/appliance-instructions.png", 
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -366,7 +392,10 @@ export const products = [
       "water boiler",
       "appliances",
       "kitchen"
-    ]
+    ], 
+    type: "appliance", 
+    instructionsLink: "images/appliance-instructions.png", 
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -671,7 +700,10 @@ export const products = [
       "coffeemakers",
       "kitchen",
       "appliances"
-    ]
+    ], 
+    type: "appliance", 
+    instructionsLink: "images/appliance-instructions.png", 
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -731,7 +763,10 @@ export const products = [
       "food blenders",
       "kitchen",
       "appliances"
-    ]
+    ], 
+    type: "appliance", 
+    instructionsLink: "images/appliance-instructions.png", 
+    warrantyLink: "images/appliance-warranty.png"
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -784,6 +819,8 @@ export const products = [
   // Convert to Clothing class using discriminator property.
   if (productDetails.type === 'clothing') {
     return new Clothing(productDetails);
+  } else if(productDetails.type === 'appliance') {
+    return new Appliance(productDetails);
   };
 
   // Convert each product object into a Product class.

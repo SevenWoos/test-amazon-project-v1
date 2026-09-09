f6fe4bb020f74cc959f36de3f96a909fca7e2616
SevenWoos <swenwu96@gmail.com>
2026-09-09 11:37:55 -0700

    Convert from promises to async await.

--- javascript-amazon-project-main/scripts/checkout.js
+++ javascript-amazon-project-main/scripts/checkout.js
@@ -6,6 +6,28 @@ import {loadCart} from '../data/cart.js';
 // import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+// Async Await = Shortcut for Promises.
+// Async makes a function return a promise.
+async function loadPage() {
+  // Await lets you write async code like normal code. Can only be used inside async function and on PROMISES.
+  await loadProductsFetch();
+
+  // Load cart
+  const value = await new Promise((resolve) => {
+    loadCart(() => {
+      resolve('value3');
+    });
+  });
+
+  // Render page after loading products and cart.
+  renderCheckoutHeader();
+  renderOrderSummary();
+  renderPaymentSummary();
+
+};
+loadPage();
+
+
 // Promises = better way to handle asynchronous code. Similar to done(). Lets us wait for code to finish before moving to next step. Helps avoid nesting and keeps code flat.
 /*
 new Promise((resolve) => {
@@ -21,6 +43,7 @@ new Promise((resolve) => {
 */
 
 // promise.all() lets us run multiple promises at the SAME TIME, and wait for all of them to finish. Use an array of promises.
+/*
 Promise.all([
   loadProductsFetch(), 
 
@@ -36,6 +59,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
 
 /*
